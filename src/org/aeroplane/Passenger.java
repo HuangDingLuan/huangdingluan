a9a6b5d1722ca66b3bce8bea27dfd374a3af191b
huangdingluan <316943766@qq.com>
2019-04-09 12:40:59 +0800

    黄定銮 3171104124 第一次上传

---
+++ src/org/aeroplane/Passenger.java
@@ -0,0 +1,42 @@
+package org.aeroplane;
+/**
+ * 旅客类
+ */
+
+public class Passenger implements PassengerInterface {
+
+	    private String names;
+	    private int bookingNumber;
+	    private int rows;
+	    private int seatPosition;
+
+	    /**
+	     * 旅客类构造方法
+	     * @param names 旅客姓名
+	     * @param bookingNumber 预定号
+	     * @param rows 座位排数
+	     * @param seatPosition 座位号
+	     */
+	    public Passenger(String names, int bookingNumber, int rows, int seatPosition) {
+	        this.names = names;
+	        this.bookingNumber = bookingNumber;
+	        this.rows = rows;
+	        this.seatPosition = seatPosition;
+	    }
+
+	    public String getName() {
+	        return names;
+	    }
+
+	    public int getBookingNumber() {
+	        return bookingNumber;
+	    }
+
+	    public int getRow() {
+	        return rows;
+	    }
+
+	    public int getSeatPosition() {
+	        return seatPosition;
+	    }
+	}
